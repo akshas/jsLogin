3a0055c7051b9c6fbaa8da00157dee7409857a9a
akshas <allexxx81@gmail.com>
2020-01-14 12:14:09 +0100

    done

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const app = express();
 app.use(express.json());
 const users = [];
 
+// routes
 
 app.get("/users", (req, res) => {
     res.json(users)
 })
+
 app.post("/users", async (req, res) => {
     try {
         const salt = await bcrypt.genSalt();
@@ -27,7 +29,29 @@ app.post("/users", async (req, res) => {
         res.status(500).send()
     }
 })
+// получение данных из базы, поэтому асинхронная функция
+app.post("/users/login", async (req, res) => {
+    // находим юзера в массиве с помощью find()
+    const user = users.find(user => user.name == req.body.name)
+    if (user == null) {
+        // если его нет отправляем статус 400
+        return res.status(400).send('Cannot find user');
+    }
+    // потом проверяем введенный пароль с помощью bcrypt.compare("введенный пароль", "пароль из базы")
+    try {
+        // bcrypt после await. Нужно ждать пока сравняться пароли из базы и введенны, и только потом res.send ...
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            res.send("Success");
+        } else {
+            res.send("Not allowed")
+        }
+    } catch (e) {
+        res.status(500).send()
+    }
+})
 
+
+// init server connection
 function init() {
     try {
         app.listen(3000, () => {
